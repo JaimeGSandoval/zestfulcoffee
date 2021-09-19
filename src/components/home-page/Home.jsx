da9ec3c5155e6bcafae9c03e357b70ae5003f28b
jaimegsandoval <jaime.devs@gmail.com>
2021-09-19 16:51:24 -0700

    added plan section & stepBox component w/ styles

--- src/components/home-page/Home.jsx
+++ src/components/home-page/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './hero-section/Hero';
 import Collection from './collection-section/Collection';
 import ChooseUs from './choose-us-section/Choose-us';
+import Plan from './plan-section/Plan';
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero />
       <Collection />
       <ChooseUs />
+      <Plan />
     </>
   );
 };
---
+++ src/components/home-page/plan-section/StepBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const StepBox = ({ step, stepNumber }) => {
+  return (
+    <div className="step-box">
+      <span className="step-number">0{`${stepNumber}`}</span>
+      <h3 className="step-title">{step.title}</h3>
+      <p className="step-description">{step.description}</p>
+    </div>
+  );
+};
+
+export default StepBox;
