49324a6421c68b8777e164b4615ab7e4ad1e0da9
jaimegsandoval <jaime.devs@gmail.com>
2021-09-19 14:49:10 -0700

    added the mobile hero component w/ styles for home page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import MobileNavModal from './components/mobile-nav-modal/mobile-nav-modal';
 import MobileHeader from './components/mobile-header/mobile-header';
+import Home from './components/home-page/Home';
 import './sass/main.scss';
 
 function App() {
@@ -11,11 +12,18 @@ function App() {
     <div className="App">
       <MobileNavModal showModal={showModal} setShowModal={setShowModal} />
       <MobileHeader setShowModal={setShowModal} />
-      <Switch>
-        <Route exact path="/" render={() => <h1>Home Page</h1>} />
-        <Route exact path="/about" render={() => <h1>About Page</h1>} />
-        <Route exact path="/plan" render={() => <h1>Plan Page</h1>} />
-      </Switch>
+
+      <div className="container">
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={() => <Home showModal={setShowModal} />}
+          />
+          <Route exact path="/about" render={() => <h1>About Page</h1>} />
+          <Route exact path="/plan" render={() => <h1>Plan Page</h1>} />
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/components/home-page/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from './hero-section/Hero.jsx';
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+    </>
+  );
+};
+
+export default Home;
