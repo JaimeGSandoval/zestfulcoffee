e002bad3ac07b630f8b832ab680d4fc2873751a5
jaimegsandoval <jaime.devs@gmail.com>
2021-09-19 21:56:22 -0700

    added plan hero and plan steps components w/ styles

---
+++ src/components/plan-page/Plan.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Hero from './hero-section/Hero';
+import PlanSteps from './plan-section/PlanSteps';
+
+const Plan = () => {
+  return (
+    <>
+      <Hero />
+      <PlanSteps />
+    </>
+  );
+};
+
+export default Plan;
