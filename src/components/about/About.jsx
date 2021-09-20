496444be95740887a6ff3044e320e8d954295eab
jaimegsandoval <jaime.devs@gmail.com>
2021-09-19 20:05:24 -0700

    added address box component & styles

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from './hero-section/Hero';
 import Commitment from './commitment-section/Commitment';
 import Quality from './quality-section/Quality';
+import Headquarters from './headquarter-section/Headquarters';
 
 const About = () => {
   return (
@@ -9,6 +10,7 @@ const About = () => {
       <Hero />
       <Commitment />
       <Quality />
+      <Headquarters />
     </>
   );
 };
