558ceda4524500092695b8ba60c2131ed62bb876
jaimegsandoval <jaime.devs@gmail.com>
2021-09-19 19:26:07 -0700

    added the commitment, hero, & quality sections

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import Hero from './hero-section/Hero';
+import Commitment from './commitment-section/Commitment';
+import Quality from './quality-section/Quality';
 
 const About = () => {
   return (
     <>
       <Hero />
+      <Commitment />
+      <Quality />
     </>
   );
 };
--- src/components/about/hero-section/Hero.jsx
+++ src/components/about/hero-section/Hero.jsx
@@ -5,13 +5,13 @@ import './_hero-about.scss';
 
 const Hero = () => {
   return (
-    <section class="hero-about-section">
+    <section className="hero-about-section">
       <picture>
         <source srcSet={whitecupWebp} />
-        <img src={whitecupJpg} alt="White cupcake" class="hero-about" />
+        <img src={whitecupJpg} alt="White cupcake" className="hero-about" />
       </picture>
-      <span class="about-us-title">about us</span>
-      <p class="about-us-description">
+      <span className="about-us-title">about us</span>
+      <p className="about-us-description">
         Coffeeroasters began it's journey of exotic discovery in 1998,
         highlighting stories of coffee from around the world. We have since been
         dedicated to bring the perfect cup - from bean to brew - in every
