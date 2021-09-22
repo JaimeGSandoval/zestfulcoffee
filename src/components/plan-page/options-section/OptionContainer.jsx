5ccbd2fd36408cd60f5eb44a8bb1733095a91146
jaimegsandoval <jaime.devs@gmail.com>
2021-09-21 21:12:30 -0700

    added new components for optionContainer & optionBox

---
+++ src/components/plan-page/options-section/OptionContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import OptionBox from './OptionBox';
+import arrowIcon from '../../../assets/icons/icon-arrow.svg';
+
+const OptionContainer = ({ optionQuestion, option }) => {
+  return (
+    <div className="option-container">
+      <h1 className="option-title-question">{optionQuestion}</h1>
+      <img src={arrowIcon} alt="Arrow icon" className="arrow" data-arrow />
+      <div className="options-container">
+        <OptionBox option={option} />
+      </div>
+    </div>
+  );
+};
+
+export default OptionContainer;
---
+++ src/components/plan-page/options-section/coffeeOptions.js
@@ -0,0 +1,104 @@
+const coffeeOptions = [
+  {
+    question: 'How do you drink your coffee?',
+    options: [
+      {
+        optionTitle: 'Capsules',
+        optionDescription:
+          'Compatible with Nespresso systems and and similar brewers',
+      },
+      {
+        optionTitle: 'Filter',
+        optionDescription:
+          'For pour over or drip methods like Aeropress, Chemex, and V60',
+      },
+      {
+        optionTitle: 'Espresso',
+        optionDescription:
+          ' Dense and finely ground beans for an intense, flavorful experience',
+      },
+    ],
+  },
+  {
+    question: 'What type of coffee?',
+    options: [
+      {
+        optionTitle: 'Single Origin',
+        optionDescription:
+          'Distinct, high quality coffee from a specific family-owned farm',
+      },
+      {
+        optionTitle: 'Decaf',
+        optionDescription:
+          'Just like regular coffee, except the caffeine has been removed',
+      },
+      {
+        optionTitle: 'Blended',
+        optionDescription:
+          'Combination of two or three dark roasted beans of organic coffees',
+      },
+    ],
+  },
+  {
+    question: 'How much would you like?',
+    options: [
+      {
+        optionTitle: '250g',
+        optionDescription:
+          'Perfect for the solo drinker. Yields about 12 delicious cups.',
+      },
+      {
+        optionTitle: '500g',
+        optionDescription:
+          'Perfect option for a couple. Yields about 40 delectable cups.',
+      },
+      {
+        optionTitle: '1000g',
+        optionDescription:
+          'Perfect for offices and events. Yields about 90 delightful cups.',
+      },
+    ],
+  },
+  {
+    question: 'Want us to grind them?',
+    options: [
+      {
+        optionTitle: 'Whole Bean',
+        optionDescription:
+          'Best choice if you cherish the full sensory experience',
+      },
+      {
+        optionTitle: 'Filter',
+        optionDescription:
+          'For pour over or drip methods like Aeropress, Chemex, and V60',
+      },
+      {
+        optionTitle: 'Cafetiére',
+        optionDescription:
+          'Course ground beans specially suited for french press coffee',
+      },
+    ],
+  },
+  {
+    question: 'How often should we deliver?',
+    options: [
+      {
+        optionTitle: 'Every week',
+        optionDescription:
+          ' $14.00 per shipment. Includes free first-className shipping.',
+      },
+      {
+        optionTitle: 'Every 2 weeks',
+        optionDescription:
+          '$17.25 per shipment. Includes free priority shipping.',
+      },
+      {
+        optionTitle: 'Every month',
+        optionDescription:
+          '$22.50 per shipment. Includes free priority shipping.',
+      },
+    ],
+  },
+];
+
+export default coffeeOptions;
