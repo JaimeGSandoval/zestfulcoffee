6330168823f2f7320475f233bc9625f30df613a6
jaimegsandoval <jaime.devs@gmail.com>
2021-10-09 22:55:43 -0700

    completed XL viewports styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import React, { useRef } from 'react';
+import React, { useRef, Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import MobileNavModal from './components/mobile-nav-modal/mobile-nav-modal';
+// import MobileNavModal from './components/mobile-nav-modal/mobile-nav-modal';
 import Header from './components/header/header';
 import Home from './components/home-page/Home';
 import About from './components/about-page/About';
 import Plan from './components/plan-page/Plan';
 import Footer from './components/footer/Footer';
 import './sass/main.scss';
+
+const MobileNavModal = lazy(() =>
+  import('./components/mobile-nav-modal/mobile-nav-modal')
+);
 
 function App() {
   const modalRef = useRef();
@@ -17,7 +21,9 @@ function App() {
 
   return (
     <div className="App">
-      <MobileNavModal ref={modalRef} />
+      <Suspense fallback={<div>...</div>}>
+        <MobileNavModal ref={modalRef} />
+      </Suspense>
       <Header openModal={openModal} />
 
       <main className="container">
