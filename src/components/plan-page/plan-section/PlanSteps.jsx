a7eb2e386298a65b223b9a24f05eb7961eed7b8f
jaimegsandoval <jaime.devs@gmail.com>
2021-12-09 21:41:13 -0800

    Relocate data for HQ & plan page plan section
    Moved the data needed for the headquarters and plan page plan section
    modules to their own data modules within their respective dirs. Makes
    for cleaner components.

--- src/components/plan-page/plan-section/PlanSteps.jsx
+++ src/components/plan-page/plan-section/PlanSteps.jsx
@@ -1,24 +1,7 @@
 import React from 'react';
 import StepBox from './StepBox';
+import steps from './data';
 import styles from './_plan-section.module.scss';
-
-const steps = [
-  {
-    title: 'Pick your coffee',
-    description:
-      'Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.',
-  },
-  {
-    title: 'Choose the frequency',
-    description:
-      'Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.',
-  },
-  {
-    title: 'Receive and enjoy!',
-    description:
-      ' We ship your product within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.',
-  },
-];
 
 const PlanSteps = () => {
   return (
