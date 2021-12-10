12113f6c2ac604f4d6d1c13568df8b0c5dd74e23
jaimegsandoval <jaime.devs@gmail.com>
2021-12-09 21:27:53 -0800

    add data modules for home page components
    Removed the data needed for rendering from the component modules in
    the home-page dir and moved them to their own modules. This makes the
    component a little cleaner while keeping encapsulation entact.

---
+++ src/components/home-page/choose-us-section/data.js
@@ -0,0 +1,26 @@
+import coffeeBean from '../../../assets/icons/icon-coffee-bean.svg';
+import gift from '../../../assets/icons/icon-gift.svg';
+import truck from '../../../assets/icons/icon-truck.svg';
+
+const perks = [
+  {
+    perkTitle: 'Best Quality',
+    perkDescription:
+      "Discover an endless variety of the world's artisan coffee from each of our roasters",
+    icon: coffeeBean,
+  },
+  {
+    perkTitle: 'Exclusive benefits',
+    perkDescription:
+      'Special offers and swag when you subscribe, including 30% off your first shipment.',
+    icon: gift,
+  },
+  {
+    perkTitle: 'Free shipping',
+    perkDescription:
+      '  We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.',
+    icon: truck,
+  },
+];
+
+export default perks;
